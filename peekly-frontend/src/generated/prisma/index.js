0ad2bc970a2a3b84b61b5d4f6d1268d1ffeb4ec8
AbolareRoheemah <roheemahabo@gmail.com>
2025-09-02 19:29:11 +0100

    added packages

--- peekly-frontend/src/generated/prisma/index.js
+++ peekly-frontend/src/generated/prisma/index.js
@@ -166,7 +166,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\Oshadhi Liyanage\\Learnings\\Dev3pack\\Peekly\\peekly-frontend\\src\\generated\\prisma",
+      "value": "/Users/roheemah/Desktop/miniapp/peekly/peekly-frontend/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -175,17 +175,16 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin-arm64",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\Oshadhi Liyanage\\Learnings\\Dev3pack\\Peekly\\peekly-frontend\\prisma\\schema.prisma",
+    "sourceFilePath": "/Users/roheemah/Desktop/miniapp/peekly/peekly-frontend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.15.0",
@@ -194,7 +193,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
@@ -243,8 +242,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "src/generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
+path.join(process.cwd(), "src/generated/prisma/libquery_engine-darwin-arm64.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "src/generated/prisma/schema.prisma")
